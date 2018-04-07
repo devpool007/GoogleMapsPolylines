535d43467dda78f453b1feb9915a01c03b4c3d3e
devpool007 <sharmadevansh007777@gmail.com>
2018-04-07 23:17:49 +0530

    Initial Commit

---
+++ MapsDataFetching/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapsDataFetching
+//
+//  Created by Devansh Sharma on 07/04/18.
+//  Copyright © 2018 Devansh Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
